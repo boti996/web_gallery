665b1013ff7f95b901963c50e27fb24795bb30ec
boti996 <dobai.botond@gmail.com>
2020-10-08 09:19:27 +0000

    Models: deleted

--- Models/ContentPageProperties.cs
+++
@@ -1,8 +0,0 @@
-namespace web_gallery.Models
-{
-    public class ContentAccessProperties
-    {
-        public Models.UserTypes MinPrivilegeLevel { get; set; }
-        public bool IsLoginRequired { get; set; }
-    } 
-}
