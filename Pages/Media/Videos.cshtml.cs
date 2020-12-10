ec76550be2f4bac46699e29ecc56dfadeef14e0d
boti996 <dobai.botond@gmail.com>
2020-12-10 08:53:56 +0000

    load Videos from mongo into the view

--- Pages/Media/Videos.cshtml.cs
+++ Pages/Media/Videos.cshtml.cs
@@ -6,22 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Logging;
+using web_gallery.Services;
 
 namespace web_gallery.Pages
 {
     [AllowAnonymous]
     public class VideosModel : PageModel
     {
+        private readonly VideoService _videoService;
         private readonly ILogger<VideosModel> _logger;
 
-        public VideosModel(ILogger<VideosModel> logger)
+        [BindProperty]
+        public List<Models.Media.Video> Videos { get; set; } = new List<Models.Media.Video>{};
+
+        public VideosModel(ILogger<VideosModel> logger, VideoService videoService)
         {
             _logger = logger;
+            _videoService = videoService;
         }
 
         public void OnGet()
         {
-
+            Videos = _videoService.Get();
         }
     }
 }
